de27555db44af53919c40071b52ed5199e128253
Ron Fernandez <ronbfernandez@gmail.com>
2021-06-07 19:16:06 -0700

    more changes to tileLayers

--- static/js/logic.js
+++ static/js/logic.js
@@ -9,17 +9,17 @@ var earthquakes = new L.LayerGroup();
 // Selectable backgrounds of our map - tile layers:
 // grayscale background.
 var graymap_background = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?" +
-  "access_token=pk.eyJ1IjoibWFudWVsYW1hY2hhZG8iLCJhIjoiY2ppczQ0NzBtMWNydTNrdDl6Z2JhdzZidSJ9.BFD3qzgAC2kMoEZirGaDjA");
+  "access_token=pk.eyJ1Ijoicm9uYmZlcm5hbmRleiIsImEiOiJja3A0c3J6ZjMwY3IyMnB0OGYwZTRpZDIzIn0.V-N2WedlsiLh5E6ybb30ng");
 
 // satellite background.
 var satellitemap_background = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/256/{z}/{x}/{y}?" +
-  "access_token=pk.eyJ1IjoibWFudWVsYW1hY2hhZG8iLCJhIjoiY2ppczQ0NzBtMWNydTNrdDl6Z2JhdzZidSJ9.BFD3qzgAC2kMoEZirGaDjA");
+  "access_token=pk.eyJ1Ijoicm9uYmZlcm5hbmRleiIsImEiOiJja3A0c3J6ZjMwY3IyMnB0OGYwZTRpZDIzIn0.V-N2WedlsiLh5E6ybb30ng");
 
 // outdoors background.
 var outdoors_background = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v9/tiles/256/{z}/{x}/{y}?" +
-  "access_token=pk.eyJ1IjoibWFudWVsYW1hY2hhZG8iLCJhIjoiY2ppczQ0NzBtMWNydTNrdDl6Z2JhdzZidSJ9.BFD3qzgAC2kMoEZirGaDjA");
+  "access_token=pk.eyJ1Ijoicm9uYmZlcm5hbmRleiIsImEiOiJja3A0c3J6ZjMwY3IyMnB0OGYwZTRpZDIzIn0.V-N2WedlsiLh5E6ybb30ng");
 
-// map object to an array of layers we created.
+  // map object to an array of layers we created.
 var map = L.map("mapid", {
   center: [37.09, -95.71],
   zoom: 5,
