0a29dd4a7b3688f9e24796d8050775905532d3fb
Ron Fernandez <ronbfernandez@gmail.com>
2021-06-07 21:34:33 -0700

    code updated to correct for api key usage in calling map layers

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,4 +1,5 @@
 // Visualizing_data-with Leaflet - logic.js
+
   // retrieve earthquake geoJSON data.
   d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson", function(data) {
 
@@ -39,15 +40,15 @@ var earthquakes = new L.LayerGroup();
 // Selectable backgrounds of our map - tile layers:
 // grayscale background.
 var graymap_background = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?" +
-  "access_token=pk.eyJ1Ijoicm9uYmZlcm5hbmRleiIsImEiOiJja3A0c3J6ZjMwY3IyMnB0OGYwZTRpZDIzIn0.V-N2WedlsiLh5E6ybb30ng");
+"access_token=pk.eyJ1Ijoicm9uYmZlcm5hbmRleiIsImEiOiJja3A0c3J6ZjMwY3IyMnB0OGYwZTRpZDIzIn0.V-N2WedlsiLh5E6ybb30ng");
 
 // satellite background.
 var satellitemap_background = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/256/{z}/{x}/{y}?" +
-  "access_token=pk.eyJ1Ijoicm9uYmZlcm5hbmRleiIsImEiOiJja3A0c3J6ZjMwY3IyMnB0OGYwZTRpZDIzIn0.V-N2WedlsiLh5E6ybb30ng");
+"access_token=pk.eyJ1Ijoicm9uYmZlcm5hbmRleiIsImEiOiJja3A0c3J6ZjMwY3IyMnB0OGYwZTRpZDIzIn0.V-N2WedlsiLh5E6ybb30ng");
 
 // outdoors background.
 var outdoors_background = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v9/tiles/256/{z}/{x}/{y}?" +
-  "access_token=pk.eyJ1Ijoicm9uYmZlcm5hbmRleiIsImEiOiJja3A0c3J6ZjMwY3IyMnB0OGYwZTRpZDIzIn0.V-N2WedlsiLh5E6ybb30ng");
+"access_token=pk.eyJ1Ijoicm9uYmZlcm5hbmRleiIsImEiOiJja3A0c3J6ZjMwY3IyMnB0OGYwZTRpZDIzIn0.V-N2WedlsiLh5E6ybb30ng");
 
   // map object to an array of layers we created.
 var map = L.map("mapid", {
