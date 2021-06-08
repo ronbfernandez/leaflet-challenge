7b5c4d39d33670a8c165410c49e955ce0377eafb
Ron Fernandez <ronbfernandez@gmail.com>
2021-06-07 21:00:14 -0700

    Code added to css and index files

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,37 @@
 // Visualizing_data-with Leaflet - logic.js
+  // retrieve earthquake geoJSON data.
+  d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson", function(data) {
 
-//Earhquakes GeoJSON URL Variable
-var earthquakesURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+    function styleInfo(feature) {
+      return {
+        opacity: 1,
+        fillOpacity: 1,
+        fillColor: getColor(feature.properties.mag),
+        color: "#000000",
+        radius: getRadius(feature.properties.mag),
+        stroke: true,
+        weight: 0.5
+      };
+    }
+  
+    // Define the color of the marker based on the magnitude of the earthquake.
+    function getColor(magnitude) {
+      switch (true) {
+        case magnitude > 5:
+          return "#ea2c2c";
+        case magnitude > 4:
+          return "#ea822c";
+        case magnitude > 3:
+          return "#ee9c00";
+        case magnitude > 2:
+          return "#eecc00";
+        case magnitude > 1:
+          return "#d4ee00";
+        default:
+          return "#98ee00";
+      }
+    }
 
 // Initialize & Create LayerGroup for earhquakes
 var earthquakes = new L.LayerGroup();
@@ -52,4 +82,73 @@ L
   .layers(baseMaps, overlayMaps)
   .addTo(map);
 
-  
+ // define the radius of the earthquake marker based on its magnitude.
+
+ function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+
+    return magnitude * 3;
+  }
+
+  // add GeoJSON layer to the map
+  L.geoJson(data, {
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng);
+    },
+    style: styleInfo,
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
+
+  }).addTo(earthquakes);
+
+  earthquakes.addTo(map);
+
+
+  var legend = L.control({
+    position: "bottomright"
+  });
+
+
+  legend.onAdd = function() {
+    var div = L
+      .DomUtil
+      .create("div", "info legend");
+
+    var grades = [0, 1, 2, 3, 4, 5];
+    var colors = [
+      "#98ee00",
+      "#d4ee00",
+      "#eecc00",
+      "#ee9c00",
+      "#ea822c",
+      "#ea2c2c"
+    ];
+
+
+    for (var i = 0; i < grades.length; i++) {
+      div.innerHTML += "<i style='background: " + colors[i] + "'></i> " +
+        grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+    }
+    return div;
+  };
+
+
+  legend.addTo(map);
+
+  // retrive Tectonic Plate geoJSON data.
+  d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json",
+    function(platedata) {
+ 
+      L.geoJson(platedata, {
+        color: "orange",
+        weight: 2
+      })
+      .addTo(tectonicplates);
+
+      // add the tectonicplates layer to the map.
+      tectonicplates.addTo(map);
+    });
+});
